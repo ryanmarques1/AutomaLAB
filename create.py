301a89eb890c451f131a66d9f1b9419a2f2c9fa0
ryanmarques <ryanmarquescastro2015@gmail.com>
2024-04-30 22:59:27 -0300

    Pequenas alterações e correções de bugs.

--- create.py
+++ create.py
@@ -1,10 +1,11 @@
-def create_afnafd():
+def create_afnafd(caracteres_especiais):
     import os
     estados = []
     alfabeto = [] # 'a , b , c'
     delta = {} #Funções de transição dicionarios.
     estado_ini = ""
     estados_finais = []
+    
 
     while 1:
         print("Menu de opções",end="\n")
@@ -17,15 +18,27 @@ def create_afnafd():
             print("Criando um AFD", end="\n")
             print('Informe o conjunto de estados: ', end="")
             estados = input().split()
+            if not estados or estados != caracteres_especiais:
+                print("Vazio ou está com caracteres não permitidos, retornando ao menu de opções.")
+                return
 
             print('Informe a linguagem do automato: ', end="")
             alfabeto = input().split()
+            if not alfabeto or alfabeto != caracteres_especiais:
+                print("Vazio, retornando ao menu de opções.")
+                return
 
             print('Informe o estado inicial: ', end="")
             estado_ini = input()
+            if estado_ini == "" or estado_ini == caracteres_especiais:
+                print("Vazio, retornando ao menu de opções.")
+                return
 
             print('Informe o(s) estado(s) finai(s): ', end="")
             estados_finais = input().split()
+            if not estados_finais or estados_finais == caracteres_especiais:
+                print("Vazio, retornando ao menu de opções.")
+                return
 
             print('Defina o delta(transição funções)', end="\n")
 
@@ -34,6 +47,9 @@ def create_afnafd():
                     print(f"\t {simbolo}")
                     print(f"{estado}\t------>\t", end="")
                     estado_prox = input()
+                    if estado_prox == "" or estado_prox == caracteres_especiais:
+                        print("Vazio ou esta com caracteres especiais, retornando ao menu de opções.")
+                        return
                     
                     if estado_prox == '-': #if para tratar casos onde um estado não atende um simbolo
                         delta[(estado, simbolo)] = None 
@@ -44,12 +60,19 @@ def create_afnafd():
             pasta_afd = "AFDs/"
             if not os.path.exists(pasta_afd):
                 os.mkdir(pasta_afd) #cria a pasta
-            arq_automato = open(pasta_afd + ("automatoAFD_criado.txt"), 'w')# coloca o arquivo dentro da pasta
-            arq_automato.writelines(str(delta)) #converte o dicionario porque metodos referentes a arquivos só aceitam strings.
-            arq_automato.close()
+            arq_automatoAFD = open(pasta_afd + ("automatoAFD_criado.txt"), 'w')# coloca o arquivo dentro da pasta
+            arq_automatoAFD.writelines(str(delta)) #converte o dicionario porque metodos referentes a arquivos só aceitam strings.
+            arq_automatoAFD.close()
             
         elif op_create == 2:
             print("Criando um AFN", end="\n")
+            
+            pasta_afn = "AFNs/"
+            if not os.path.exists(pasta_afn):
+                os.mkdir(pasta_afn) #Cria a pasta
+            arq_automatoAFN = open(pasta_afn + ("automatoAFN_criado.txt"), 'w')
+            arq_automatoAFN.writelines(str(delta))
+            arq_automatoAFN.close()
 
         elif op_create == 3:
             print("Informe a linguagem a ser reconhecida: ", end="") 
@@ -75,7 +98,6 @@ def create_afnafd():
             else:
                 print("Nao reconheceu!")
             
-                
         elif op_create == 4:
             print("Voltando para o menu principal.", end="\n")
             break
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import create
 import converte
 import minimiza
+
+#Variaveis para controle de acesso indeterminado
+caracteres_especiais = "!@#$%&*()-+=<>:;^~.,][}{?/"
+
+#Variaveis para controle de acesso indeterminado
 print('Bem vindo ao Sistema de AFNS e AFDS')
 
 
 while 1:
-    print("Menu de opções",end="\n")
+    print("Menu Principal",end="\n")
     print(" 1. Criar e Testar",end="\n")
     print(" 2. Converter um AFN -> AFD",end="\n")
     print(" 3. Minimizar um AFD", end="\n")
     print(" 4. Sair",end="\n")
     op = int(input("Entre com a opção desejada: "))
     if op == 1:
-        create.create_afnafd()
+        create.create_afnafd(caracteres_especiais)
         continue
     elif op == 2:
         converte.converte_afnafd()
